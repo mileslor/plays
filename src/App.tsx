b1e9cfae99b6a49486955557c876b943458518b7
mileslor <65813317+mileslor@users.noreply.github.com>
2026-07-02 22:36:08 +0100

    perf: lazy-load game pages to reduce initial bundle (530→368KB)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
+import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
-import Undercover from './pages/Undercover'
-import Werewolf from './pages/Werewolf'
-import BlackMagic from './pages/BlackMagic'
-import NumberGuess from './pages/NumberGuess'
-import WheelOfFortune from './pages/WheelOfFortune'
-import Ludo from './pages/Ludo'
-import Chess from './pages/Chess'
-import CelebrityGuess from './pages/CelebrityGuess'
+
+const Undercover = lazy(() => import('./pages/Undercover'))
+const Werewolf = lazy(() => import('./pages/Werewolf'))
+const BlackMagic = lazy(() => import('./pages/BlackMagic'))
+const NumberGuess = lazy(() => import('./pages/NumberGuess'))
+const WheelOfFortune = lazy(() => import('./pages/WheelOfFortune'))
+const Ludo = lazy(() => import('./pages/Ludo'))
+const Chess = lazy(() => import('./pages/Chess'))
+const CelebrityGuess = lazy(() => import('./pages/CelebrityGuess'))
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout><Home /></Layout>} />
-      <Route path="/undercover" element={<Undercover />} />
-      <Route path="/werewolf" element={<Werewolf />} />
-      <Route path="/black-magic" element={<BlackMagic />} />
-      <Route path="/number-guess" element={<NumberGuess />} />
-      <Route path="/wheel" element={<WheelOfFortune />} />
-      <Route path="/ludo" element={<Ludo />} />
-      <Route path="/chess" element={<Chess />} />
-      <Route path="/celebrity" element={<CelebrityGuess />} />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/undercover" element={<Undercover />} />
+        <Route path="/werewolf" element={<Werewolf />} />
+        <Route path="/black-magic" element={<BlackMagic />} />
+        <Route path="/number-guess" element={<NumberGuess />} />
+        <Route path="/wheel" element={<WheelOfFortune />} />
+        <Route path="/ludo" element={<Ludo />} />
+        <Route path="/chess" element={<Chess />} />
+        <Route path="/celebrity" element={<CelebrityGuess />} />
+      </Routes>
+    </Suspense>
   )
 }
